b679a7a9564e51263d8561c20209433a5add846e
TR742 <Tyrich1442@gmail.com>
2022-06-20 12:55:15 -0700

    updated models

--- models/index.js
+++ models/index.js
@@ -1,2 +1,15 @@
-const User = require('./user');
-const Post = require('./post');
+const User = require('./User')
+const Comment = require('./Comment')
+
+// Define the above associate from the opposite end. 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Define a User as having many Recipes
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Comment };

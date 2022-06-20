66a00b8037ddb4402341cee080776d2fcadf7c5f
TR742 <Tyrich1442@gmail.com>
2022-06-20 12:54:17 -0700

    Updated db name/server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const path = require('path');
-const helpers = require('./utils/helpers');
 const routes = require('./controllers');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
@@ -33,6 +33,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false}).then(() => {
-  app.listen(PORT, () => console.log('Now listening to ', PORT ));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });

ff4f1c514be3af1f61641b2542a4a5a26023e303
TR742 <Tyrich1442@gmail.com>
2022-06-20 12:55:37 -0700

    seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,2 +1,22 @@
-const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Comment } = require("../models");
+
+const userData = require("./userData.json");
+const commentData = require("./commentData.json");
+
+const seedData = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const comments = await Comment.bulkCreate(commentData, {});
+  
+  console.log('working')
+  
+  process.exit(0);
+};
+
+seedData();
